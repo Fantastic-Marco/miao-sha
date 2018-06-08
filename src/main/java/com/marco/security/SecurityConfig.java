c6bc6462b9e74491eff3fedfb7a5148e193c5b56
Fantastic-Marco <853707815@qq.com>
2018-06-08 09:37:25 +0800

    更改字符编码

--- src/main/java/com/marco/security/SecurityConfig.java
+++ src/main/java/com/marco/security/SecurityConfig.java
@@ -1,9 +1,12 @@
 //package com.marco.security;
 //
+//import com.alibaba.druid.pool.DruidDataSource;
 //import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
 //
 ///**
 // * Created by landun on 2018/5/25.
@@ -12,13 +15,15 @@
 //public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //    @Autowired
-//    public void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .httpBasic();
+//    private DruidDataSource dataSource;
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .withDefaultSchema()
+//                .withUser("user").password("password").roles("USER").and()
+//                .withUser("admin").password("password").roles("USER", "ADMIN");
 //    }
 //}
